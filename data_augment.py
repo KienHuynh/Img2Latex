adc261876672f5f93009412f636595fde9ddab2a
KienHuynh <hckien@hcmut.edu.vn>
2018-02-06 17:25:25 +0700

    Refactor codes, add doc strings and warning

--- data_augment.py
+++ data_augment.py
@@ -1,28 +1,44 @@
+"""
+@author: Kien Huynh
+
+This module contains some necessary random augmentation methods which can be used in our math expression recognization training.
+"""
+
 import numpy as np
-import scipy.misc
+import scipy.misc, scipy.ndimage
 from skimage.color import rgb2hsv, hsv2rgb
 
-import pdb
+import pdb, traceback
 
 def gray2rgb(img):
     """gray2rgb
-    Convert a gray image to rgb
+    Convert a gray image to rgb.
 
-    :param img: numpy array, gray image of shape (H, W)
+    :param img: numpy array, gray image of shape (H, W).
     """
     imh, imw = img.shape
     img = np.reshape(img, (imh, imw, 1))
     return np.concatenate((img, img, img), axis = 2)
 
+def invert_img(img):
+    """invert_img
+    Invert the input image.
+
+    :param img: numpy array, the image.
+    """
+    if (np.max(img) > 1.0 or np.min(img) < 0.0):
+        print("Warning: invert_img expects an image with values in the [0,1] interval")
+    return 1.0 - img
+
 
 def random_scale(img, min_scale, max_scale, min_pad):
     """random_scale
-    This function scales the input image using a random float value ranged from min_scale to max_scale then crops it back to the original size
+    This function scales the input image using a random float value ranged from min_scale to max_scale then crops it back to the original size.
 
-    :param img: numpy array, image
-    :min_scale: float, minimum scale
-    :max_scale: float, maximum scale
-    :min_pad: minimum number of pixels to be padded around the equation (after cropping)
+    :param img: numpy array, image.
+    :min_scale: float, minimum scale.
+    :max_scale: float, maximum scale.
+    :min_pad: minimum number of pixels to be padded around the equation (after cropping).
     """
     imh, imw, _ = img.shape
     
@@ -56,7 +72,7 @@ def random_scale(img, min_scale, max_scale, min_pad):
     y_max = row_sum[-1]
     x_min = col_sum[0]
     x_max = col_sum[-1]
-    #pdb.set_trace()
+    
     if (y_max - y_min) + 2 * min_pad > imh or (x_max - x_min) + 2 * min_pad > imw:
         return img
 
@@ -87,10 +103,13 @@ def random_scale(img, min_scale, max_scale, min_pad):
 
 def random_hue(img):
     """random_hue
-    Change the hue of the symbols in the image
-    
-    :param img: numpy array, image
+    Change the hsv triplet of the symbols in the image.
+
+    :param img: numpy array, image.
     """
+    if (np.max(img) > 1.0 or np.min(img) < 0.0):
+        print("Warning: random_hue expects an image with values in the [0,1] interval")
+
     num_zero = np.sum(img == 0)
     num_one = np.sum(img == 1)
     
@@ -100,7 +119,7 @@ def random_hue(img):
         bg_value = 0
 
     bg_pixel_ind = img == bg_value
-    
+   
     img_rgb = (img * 255).astype(np.uint8)
     img_hsv = rgb2hsv(img_rgb)
     hue = np.random.uniform(0, 1)
@@ -122,3 +141,16 @@ def random_hue(img):
 
     #pdb.set_trace()
     return img_rgb
+
+
+def random_rotate(img, angle_std):
+    """random_rotate
+    Rotate the image and resize it to the original size (there will be some skewing side-effect).
+    
+    :param img: numpy array, image.
+    :param angle_std: float. The actual angle will be drawn from a normal distribution with zero mean and the specified standard deviation. Note that the angle here is in degrees.
+    """
+    imh, imw, _ = img.shape
+    angle = angle_std*np.random.randn(1)
+    img = scipy.ndimage.interpolation.rotate(img, angle)
+    return scipy.misc.imresize(img, [imh, imw])
