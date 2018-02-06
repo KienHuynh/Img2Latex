980ba1de22c3a3e1786ab9c7d92dec8bfa37442f
KienHuynh <hckien@hcmut.edu.vn>
2018-02-06 18:50:58 +0700

    Add random_transform and fix image range bugs

--- data_augment.py
+++ data_augment.py
@@ -7,6 +7,7 @@ This module contains some necessary random augmentation methods which can be use
 import numpy as np
 import scipy.misc, scipy.ndimage
 from skimage.color import rgb2hsv, hsv2rgb
+from skimage import exposure
 
 import pdb, traceback
 
@@ -26,12 +27,10 @@ def invert_img(img):
 
     :param img: numpy array, the image.
     """
-    if (np.max(img) > 1.0 or np.min(img) < 0.0):
-        print("Warning: invert_img expects an image with values in the [0,1] interval")
-    return 1.0 - img
+    return 255 - img
 
 
-def random_scale(img, min_scale, max_scale, min_pad):
+def random_scale(img, min_scale, max_scale, min_pad=0):
     """random_scale
     This function scales the input image using a random float value ranged from min_scale to max_scale then crops it back to the original size.
 
@@ -107,20 +106,17 @@ def random_hue(img):
 
     :param img: numpy array, image.
     """
-    if (np.max(img) > 1.0 or np.min(img) < 0.0):
-        print("Warning: random_hue expects an image with values in the [0,1] interval")
-
     num_zero = np.sum(img == 0)
-    num_one = np.sum(img == 1)
+    num_255 = np.sum(img == 255)
     
     # Decide background value
-    bg_value = 1
-    if (num_zero > num_one):
+    bg_value = 255.0
+    if (num_zero > num_255):
         bg_value = 0
 
     bg_pixel_ind = img == bg_value
    
-    img_rgb = (img * 255).astype(np.uint8)
+    img_rgb = img.astype(np.uint8)
     img_hsv = rgb2hsv(img_rgb)
     hue = np.random.uniform(0, 1)
     sat = np.random.uniform(0, 1)
@@ -136,11 +132,12 @@ def random_hue(img):
     img_hsv[:,:,2] = val
     img_hsv[img_hsv > 1] = 1
     img_hsv[img_hsv < 0] = 0
+
+    # This hsv2rgb convert to [0,1] range instead of [0,255] for some reasons
     img_rgb = hsv2rgb(img_hsv)
     img_rgb[bg_pixel_ind] = bg_value
-
-    #pdb.set_trace()
-    return img_rgb
+ 
+    return img_rgb*255
 
 
 def random_rotate(img, angle_std):
@@ -153,4 +150,52 @@ def random_rotate(img, angle_std):
     imh, imw, _ = img.shape
     angle = angle_std*np.random.randn(1)
     img = scipy.ndimage.interpolation.rotate(img, angle)
-    return scipy.misc.imresize(img, [imh, imw])
+
+    img = scipy.misc.imresize(img, [imh, imw], interp='bicubic')
+    return img
+
+
+def random_transform(img, original=0.2, invert=0.5, scale=1.0, min_scale=0.7, max_scale=1.43, hue=1.0, rotate=1.0, angle_std=15):
+    """random_transform
+    Randomly use the random transformation defined above on img.
+
+    :param img: numpy array, image.
+    :param original: float, [0, 1], the chance to keep img as original. If the image is to be kept as original, the transformations will be ignored.
+    :param invert: float, [0, 1], the chance to invert the image.
+    :param scale: float, [0, 1], the chance to randomly scale the image.
+    :param min_scale: float, min_scale parameter in the random_scale function.
+    :param max_scale: float, max_scale parameter in the random_scale function.
+    :param hue: float, [0, 1], the chance to randomly change the HSV values of the image.
+    :param rotate: float, [0, 1], the chance to randomly rotate the image.
+    :param angle_std: float, angle_std parameter in the random_rotate function.
+    """
+    dice = np.random.uniform(0, 1.0)
+    if (dice < original):
+        return img.astype(np.uint8)
+    
+    img_trf = np.copy(img)
+   
+    #dice = np.random.uniform(0, 1.0)
+    #if (dice < invert):
+    #    img_trf = invert_img(img_trf)
+
+    dice = np.random.uniform(0, 1.0) 
+    if (dice < scale):
+        img_trf = random_scale(img_trf, min_scale, max_scale)
+    
+    dice = np.random.uniform(0, 1.0)
+    if (dice < hue):
+        img_trf = random_hue(img_trf)
+
+    dice = np.random.uniform(0, 1.0)
+    if (dice < rotate):
+        img_trf = random_rotate(img_trf, angle_std)
+        
+    dice = np.random.uniform(0, 1.0)
+    if (dice < invert):
+        img_trf = invert_img(img_trf)
+        img_trf[img_trf > 200] = 255
+    else:
+        img_trf[img_trf < 55] = 0
+
+    return img_trf
