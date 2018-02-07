7c09c0af8a077b1d9a29f770c96952a299fc5188
KienHuynh <hckien@hcmut.edu.vn>
2018-02-08 00:57:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
+# Network settings
 MAX_TOKEN_LEN = 90
 NUM_OF_TOKEN = 114
 BATCH_SIZE = 1
 IMH = 256
 IMW = 512
 
+# Training settings
 NUM_EPOCH = 50
+LR = 0.0001
 
-DATASET_PATH = './../data/TrainINKML/'
+CUDA = True
+
+# Path settings
+DATASET_PATH = './../data/'
 MODEL_FOLDER = './model/train/'
