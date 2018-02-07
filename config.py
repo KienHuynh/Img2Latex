ae2cef3c0bcfaa91ee9a6808acbab1f84efbc579
KienHuynh <hckien@hcmut.edu.vn>
2018-02-08 01:26:40 +0700

    Add batch size, subset list and scale factors

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 # Network settings
 MAX_TOKEN_LEN = 90
 NUM_OF_TOKEN = 114
-BATCH_SIZE = 1
 IMH = 256
 IMW = 512
 
 # Training settings
 NUM_EPOCH = 50
 LR = 0.0001
+BATCH_SIZE = 16
 
 CUDA = True
 
 # Path settings
 DATASET_PATH = './../data/'
 MODEL_FOLDER = './model/train/'
+
+# Subdataset settings
+SUBSET_LIST = ['expressmatch/', 'HAMEX/', 'KAIST/', 'MathBrush/', 'MfrDB/']
+# These are the approprirate scale factors of equations came from the corresponding sub-datasets (computed using CROHME_parser.size_statistics), they are used to render numpy images from inkml data
+SCALE_FACTORS = [1.0, 100, 0.065, 0.04, 0.8] 
