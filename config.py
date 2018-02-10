cf7c628d9937ec8adc80d900cc9ce1e70edcdeae
KienHuynh <hckien@hcmut.edu.vn>
2018-02-10 13:56:43 +0700

    Add more options

--- config.py
+++ config.py
@@ -5,20 +5,24 @@ IMH = 256
 IMW = 512
 
 # Training settings
-NUM_EPOCH = 50
+NUM_EPOCH = 500
 LR = 0.0001
-LR_DECAY = 1/1.15
+LR_DECAY = 1/1.5
+NUM_EPOCH_TO_DECAY = 10
 MOMENTUM = 0.9
 MAX_GRAD_CLIP = 0.1
 BATCH_SIZE = 12
+RAND_TRANSFORM = True
 
 # Saving
-NUM_EPOCH_TO_SAVE = 2 # Save every SAVE_STEP epoch
-SAVE_NAME = 'attend_gru_021018' # MMDDYY
+NUM_EPOCH_TO_SAVE = 5 # Save every SAVE_STEP epoch
+SAVE_NAME = 'attend_gru_021018_weight' # MMDDYY
 META_NAME = 'attend_gru_021018_meta'
 
 # For traking/displaying
-NUM_ITE_TO_LOG = 1 # Display information (loss, current epoch, etc.) every PRINT_STEP iteration
+NUM_ITE_TO_LOG = 10 # Display information (loss, current epoch, etc.) every PRINT_STEP iteration
+NUM_ITE_TO_VIS = 250
+VIS_PATH = './../vis/Image2Latex/'
 
 CUDA = True
 
