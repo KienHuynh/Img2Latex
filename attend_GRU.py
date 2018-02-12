ebdda25e5231dfa2e7498697cd403e20a3de5c23
KienHuynh <hckien@hcmut.edu.vn>
2018-02-12 14:28:25 +0700

    Rename variables

--- attend_GRU.py
+++ attend_GRU.py
@@ -15,17 +15,13 @@ import pdb
 import scipy.misc
 
 class AGRU(nn.Module):
-    def __init__(self):
-        self.step = 0
-        #######################################################
-        ## PARAMETER DIFINITION
+    def __init__(self):        
+        # GRU settings
         self.gru_hidden_size = 256
         self.embed_dimension = 256
         self.Q_height = 256
         self.va_len = 512
-        #######################################################
-        ## NETWORK STRUCTURE
-    
+        
         super(AGRU, self).__init__()
         self.conv1_1 = nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=False)
         self.conv1_1_bn = nn.BatchNorm2d(32)
@@ -83,7 +79,7 @@ class AGRU(nn.Module):
         # Expect size: 1 x 128 (1 x self.gru_hidden_size)
         # The hard code "128" down there is the Nc of FCN Result
         
-        #self.embeds = nn.Embedding(cfg.NUM_OF_TOKEN, self.embed_dimension) 
+        # Convolutional layers
         self.embeds_temp = nn.Linear(cfg.NUM_OF_TOKEN, self.embed_dimension) 
         self.FC_Wyz = nn.Linear(self.embed_dimension, self.gru_hidden_size)
         self.FC_Uhz = nn.Linear(self.gru_hidden_size, self.gru_hidden_size)
@@ -101,32 +97,17 @@ class AGRU(nn.Module):
         self.FC_Wh = nn.Linear(self.gru_hidden_size, self.embed_dimension) # for (11)
         self.FC_Wc = nn.Linear(128, self.embed_dimension) #
         
-
-        ###############################################
-        ########### GRU ###############################
-        ###############################################
-
-        
-        self.Coverage_MLP_From_H = nn.Linear(self.gru_hidden_size, self.va_len)
-        self.Coverage_MLP_From_A = nn.Linear(128, self.va_len)
-        self.Coverage_MLP_From_Beta = nn.Linear(self.Q_height, self.va_len)
+        # GRU layers
+        self.coverage_mlp_h = nn.Linear(self.gru_hidden_size, self.va_len)
+        self.coverage_mlp_a = nn.Linear(128, self.va_len)
+        self.coverage_mlp_beta = nn.Linear(self.Q_height, self.va_len)
 
         self.Va_fully_connected = nn.Linear(self.va_len, 1)
-        
         self.conv_Q_beta = nn.Conv2d(1, self.Q_height, 3, stride=1, padding=1, bias=False) 
-        
-        
         self.alpha_softmax = torch.nn.Softmax(dim=1)
-        #self.testnn = nn.Linear(65536, 128)
-        
 
-        self.word_to_id, self.id_to_word = get_gt.build_vocab('./mathsymbolclass.txt')
-       
- 
-    def setGroundTruth(self, GT):
-        self.GT = GT
+        self.word_to_id, self.id_to_word = get_gt.build_vocab('./mathsymbolclass.txt')              
 
-        
     def forward(self, x, target):         
         x_np = np.transpose(x.data.cpu().numpy()[0,:,:,:], (1,2,0))
         # Check if the model is running on a CUDA device
@@ -172,42 +153,50 @@ class AGRU(nn.Module):
         fcn_width = fcn_output_shape[3]
 
         num_of_block = fcn_height * fcn_width
-        ################ DEFINITION ########################################
         
+        # Create the starting vector as input for the GRU (It is the token <s>)
         start_vector = np.zeros((batch_size,1,cfg.NUM_OF_TOKEN))
         start_vector[:,0,self.word_to_id['<s>']] = 1
+       
+        # Creating necessary torch tensor Variable for later usage.
+        # GRU_hidden: hidden vector of the GRU at the start.
+        # return_tensor: the prediction of mathematical expression.
+        # alpha_mat: initial attention map, average over the FCN output.
+        # beta_mat: accumulate attention over multiple time steps.
+        # all_alpha_mat: storing alpha matrices over all time steps, used for visualization purpose.
+        # target: the label of the math expression. It is used in training only .
         if use_cuda:
             GRU_hidden = Variable(torch.FloatTensor(batch_size, self.gru_hidden_size).cuda().zero_())
             return_tensor = Variable(torch.FloatTensor(start_vector).cuda(), requires_grad=True)
             #Init alpha and beta Matrix
             alpha_mat = Variable(torch.FloatTensor(batch_size, fcn_height, fcn_width).cuda().fill_(1.0 / float(num_of_block)), requires_grad=True)
             beta_mat = Variable(torch.FloatTensor(batch_size, fcn_height, fcn_width).cuda().zero_(), requires_grad=True)
-            self.alpha_mat = Variable(torch.zeros(batch_size, 1, fcn_height,fcn_width).cuda())
+            all_alpha_mat = Variable(torch.zeros(batch_size, 1, fcn_height,fcn_width).cuda())
+            target = np.reshape(target.data.cpu(), (batch_size, cfg.MAX_TOKEN_LEN))
         else:
-            #GRU_hidden = Variable(torch.FloatTensor(batch_size, 128))
             GRU_hidden = Variable(torch.FloatTensor(batch_size, self.gru_hidden_size).zero_())
-            # Init return tensor (the prediction of mathematical Expression)
             return_tensor = Variable(torch.FloatTensor(start_vector), requires_grad=True)
             #Init Alpha and Beta Matrix
             alpha_mat = Variable(torch.FloatTensor(batch_size, fcn_height, fcn_width).fill_(1.0 / float(num_of_block)), requires_grad=True)
             beta_mat = Variable(torch.FloatTensor(batch_size, fcn_height, fcn_width).zero_(), requires_grad=True)
-            self.alpha_mat = Variable(torch.zeros(batch_size, 1, fcn_height,fcn_width))
-        
-        FCN_Straight = fcn_result.permute(0,2,3,1).contiguous()
-        FCN_Straight = FCN_Straight.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
+            all_alpha_mat = Variable(torch.zeros(batch_size, 1, fcn_height,fcn_width))
+            target = np.reshape(target.data, (batch_size, cfg.MAX_TOKEN_LEN))        
 
-        from_h = self.Coverage_MLP_From_H(GRU_hidden.view(batch_size, self.gru_hidden_size))
+        fcn_flat = fcn_result.permute(0,2,3,1).contiguous()
+        fcn_flat = fcn_flat.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
+
+        from_h = self.coverage_mlp_h(GRU_hidden.view(batch_size, self.gru_hidden_size))
         from_h = from_h.view(batch_size, self.va_len, 1, 1)
         from_h = from_h.repeat(1, 1, fcn_height, fcn_width)
 
-        from_a = self.Coverage_MLP_From_A(FCN_Straight)
+        from_a = self.coverage_mlp_a(fcn_flat)
         from_a = from_a.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
         
         F_ = self.conv_Q_beta(torch.unsqueeze(beta_mat, dim = 1)) #(13)
-        F_Straight = F_.transpose(1,3).contiguous()
-        F_Straight = F_.permute(0,2,3,1).contiguous()
-        F_Straight = F_Straight.view(batch_size * fcn_height * fcn_width, self.Q_height)
-        from_b = self.Coverage_MLP_From_Beta(F_Straight)
+        f_flat = F_.transpose(1,3).contiguous()
+        f_flat = F_.permute(0,2,3,1).contiguous()
+        f_flat = f_flat.view(batch_size * fcn_height * fcn_width, self.Q_height)
+        from_b = self.coverage_mlp_beta(f_flat)
         from_b = from_b.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
                 
         from_a = from_a + from_b + from_h
@@ -218,17 +207,12 @@ class AGRU(nn.Module):
         
         alpha_mat = alpha_mat.transpose(0,1).contiguous().view(batch_size, fcn_height, fcn_width) 
         alpha_mat = self.alpha_softmax(alpha_mat.view(batch_size, 512)).view(batch_size, fcn_height, fcn_width)
-        self.alpha_mat = torch.cat([self.alpha_mat, torch.unsqueeze(alpha_mat, dim = 1)], 1)
+        all_alpha_mat = torch.cat([all_alpha_mat, torch.unsqueeze(alpha_mat, dim = 1)], 1)
             
-        ####################################################################
-        ################ GRU BLOCK #########################################
-        ####################################################################
-
         ################### START GRU ########################
  
         # Get last predicted symbol: This will be used for GRU's input
         return_vector = torch.squeeze(return_tensor, dim = 1)
-        #GRU_output = Variable(return_tensor.data[:, 0, :])
         
         ####################################################################
         ################ GRU ITERATION #####################################
@@ -237,44 +221,21 @@ class AGRU(nn.Module):
         self.print_alpha_mat =[]
         
         # Reshape target vector so that it has the shape (batch_size, MAX_TOKEN_LEN)
-        if (use_cuda):
-            try:
-                target = np.reshape(target.data.cpu(), (batch_size, cfg.MAX_TOKEN_LEN))
-            except ValueError as e:
-                pdb.set_trace()
-        else:
-            target = np.reshape(target.data, (batch_size, cfg.MAX_TOKEN_LEN))        
-
-        for RNN_iterate in range (cfg.MAX_TOKEN_LEN - 1):
-
-            #print (alpha_mat.cpu().data.numpy().shape)
-        
+                    
+        for RNN_iterate in range (cfg.MAX_TOKEN_LEN - 1):       
             # Clone of fcn_result: We will use this for generating Ct Vector || Deprecated - We use another approach now!
             multiplied_mat = fcn_result.clone()
-
-            # Element-wise multiply between alpha and fcn_result
-            #--------
-            #for batch_index in range(batch_size):
-            #    for i in range (fcn_output_shape[1]):
-            #        multiplied_mat[batch_index][i] = multiplied_mat[batch_index][i] * alpha_mat[batch_index]
-            #-------- # alpha : batch x 16 x 32
+        
             expanded_alpha_mat = alpha_mat.view(batch_size, 1, fcn_height, fcn_width)
             expanded_alpha_mat = expanded_alpha_mat.repeat(1, fcn_output_shape[1], 1, 1)
             multiplied_mat = multiplied_mat * expanded_alpha_mat
-            #--------
-            #mytemp = Variable(alpha_mat.expand(fcn_output_shape).data)
-            #expanded_alpha_mat = mytemp
-            #multiplied_mat = multiplied_mat * expanded_alpha_mat
-                    
+                           
             # Sum all vector after element-wise multiply to get Ct
-
             multiplied_mat = torch.sum(multiplied_mat, keepdim=True, dim = 2)
             multiplied_mat = torch.sum(multiplied_mat, keepdim=True, dim = 3)
-            #multiplied_mat = self.testnn(multiplied_mat.view(batch_size, 65536))
             
             multiplied_mat = multiplied_mat.view(batch_size, 128)
-            
-            
+           
             ########################################################################################
             ################### GRU SECTION ########################################################
             ########################################################################################
@@ -368,22 +329,22 @@ class AGRU(nn.Module):
 
 
             # Get Input from h(t - 1)    
-            from_h = self.Coverage_MLP_From_H(GRU_hidden.view(batch_size, self.gru_hidden_size))
+            from_h = self.coverage_mlp_h(GRU_hidden.view(batch_size, self.gru_hidden_size))
             from_h = from_h.view(batch_size, self.va_len, 1, 1)
             from_h = from_h.repeat(1, 1, fcn_height, fcn_width)
-            #from_h = self.Coverage_MLP_From_H(torch.squeeze(GRU_hidden, dim = 1))
+            #from_h = self.coverage_mlp_h(torch.squeeze(GRU_hidden, dim = 1))
 
             # New Approach
-            FCN_Straight = fcn_result.permute(0,2,3,1).contiguous()
-            FCN_Straight = FCN_Straight.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
-            from_a = self.Coverage_MLP_From_A(FCN_Straight)
+            fcn_flat = fcn_result.permute(0,2,3,1).contiguous()
+            fcn_flat = fcn_flat.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
+            from_a = self.coverage_mlp_a(fcn_flat)
             from_a = from_a.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
             # --
             F_ = self.conv_Q_beta(torch.unsqueeze(beta_mat, dim = 1)) #(13)
-            #F_Straight = F_.transpose(1,3).contiguous()
-            F_Straight = F_.permute(0,2,3,1).contiguous()
-            F_Straight = F_Straight.view(batch_size * fcn_height * fcn_width, self.Q_height)
-            from_b = self.Coverage_MLP_From_Beta(F_Straight)
+            #f_flat = F_.transpose(1,3).contiguous()
+            f_flat = F_.permute(0,2,3,1).contiguous()
+            f_flat = f_straight.view(batch_size * fcn_height * fcn_width, self.Q_height)
+            from_b = self.coverage_mlp_beta(f_flat)
             from_b = from_b.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
             
             #---------------
@@ -402,25 +363,6 @@ class AGRU(nn.Module):
             
         
             
-            # update alpha matrix
-            #for batch_index in range(batch_size):
-            #    for i in range(fcn_height):
-            #        for j in range(fcn_width):
-            #            
-            #            #t = torch.transpose(alpha_mat.view(3,4),0,1)
-            #        # this is a_i vector
-            #            temp_tensor = Variable(torch.unsqueeze(fcn_result.data[batch_index,:,i,j], dim = 0))
-            #            
-            #            # get input from fcn_result
-            #            from_a = self.Coverage_MLP_From_A(temp_tensor)
-            #
-            #            # Assign pixel by pixel to alpha matrix
-            #            alpha_mat.data[batch_index][i][j] = from_h.data[batch_index][0] + from_a.data[0][0]
-
-            #
-            #            print (alpha_mat.data.numpy().shape)
-            
-            #alpha_mat = alpha_mat.view(1,16, 32)
             alpha_mat = F.tanh(from_a)
             
             ## Va fix ##
@@ -434,12 +376,12 @@ class AGRU(nn.Module):
             alpha_mat = self.alpha_softmax(alpha_mat.view(batch_size, 512)).view(batch_size, fcn_height, fcn_width)
 
             self.print_alpha_mat.append(alpha_mat.cpu().data.numpy())
-            self.alpha_mat = torch.cat([self.alpha_mat, torch.unsqueeze(alpha_mat, dim = 1)], 1)
+            all_alpha_mat = torch.cat([all_alpha_mat, torch.unsqueeze(alpha_mat, dim = 1)], 1)
             
         #return torch.unsqueeze(return_tensor, dim = 1)
         # Returnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn ! after a long long way :'(((((
 
-        return return_tensor, self.alpha_mat
+        return return_tensor, all_alpha_mat
 
 
     def forwardTest(self, x, beam_size = 10):
@@ -645,20 +587,19 @@ class AGRU(nn.Module):
 
 
                 # Get Input from h(t - 1)
-                from_h = self.Coverage_MLP_From_H(GRU_hidden.view(batch_size, self.gru_hidden_size))
-                #from_h = self.Coverage_MLP_From_H(torch.squeeze(GRU_hidden, dim = 1))
+                from_h = self.coverage_mlp_h(GRU_hidden.view(batch_size, self.gru_hidden_size))
 
                 # New Approach
-                FCN_Straight = fcn_result.permute(0,2,3,1).contiguous()
-                FCN_Straight = FCN_Straight.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
-                from_a = self.Coverage_MLP_From_A(FCN_Straight)
+                fcn_flat = fcn_result.permute(0,2,3,1).contiguous()
+                fcn_flat = fcn_flat.view(batch_size * fcn_height * fcn_width, fcn_output_shape[1])
+                from_a = self.coverage_mlp_a(fcn_flat)
                 from_a = from_a.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
                 # --
                 F_ = self.conv_Q_beta(torch.unsqueeze(beta_mat, dim = 1)) #(13)
-                #F_Straight = F_.transpose(1,3).contiguous()
-                F_Straight = F_.permute(0,2,3,1).contiguous()
-                F_Straight = F_Straight.view(batch_size * fcn_height * fcn_width, self.Q_height)
-                from_b = self.Coverage_MLP_From_Beta(F_Straight)
+                #f_flat = F_.transpose(1,3).contiguous()
+                f_flat = F_.permute(0,2,3,1).contiguous()
+                f_flat = f_straight.view(batch_size * fcn_height * fcn_width, self.Q_height)
+                from_b = self.coverage_mlp_beta(f_flat)
                 from_b = from_b.transpose(0,1).contiguous().view(batch_size, self.va_len, fcn_height, fcn_width)
                 
                 #---------------
