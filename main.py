733c38f4fe638abb78b54dfabe953412b0ca4e8e
KienHuynh <hckien@hcmut.edu.vn>
2018-02-13 23:10:06 +0700

    Implement test function

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from torch.autograd import Variable
 from attend_GRU import AGRU
 import util
 
+import os
 import glob
+import ntpath
 import pickle
 
 import pdb
@@ -140,7 +142,7 @@ def train():
             if (batch_idx[-1] >= num_train):
                 batch_idx = range(i*batch_size, num_train)
             batch_size = len(batch_idx) 
-            batch_x = util.batch_data(inkml_list[batch_idx], scale_list[batch_idx], True)
+            batch_x = util.batch_data(inkml_list[batch_idx], scale_list[batch_idx], is_train)
             batch_x = util.np_to_var(batch_x, use_cuda)
             batch_y_np = util.batch_target(inkml_list[batch_idx])
             batch_y = util.np_to_var(batch_y_np, use_cuda)
@@ -209,8 +211,139 @@ def train():
             util.save_list(metadata, save_path + meta_name + ('_%03d' % e) + '.dat')
              
         last_e = e
+
+
+def test():
+    # Getting settings from config.py
+    max_len = cfg.MAX_TOKEN_LEN
+    num_token = cfg.NUM_OF_TOKEN
+    imw = cfg.IMW
+    imh = cfg.IMH    
+    
+    # Training params
+    is_train = False
+    batch_size = cfg.BATCH_SIZE
+
+    # Tracking/Saving
+    num_ite_to_log = cfg.NUM_ITE_TO_LOG
+    num_ite_to_vis = cfg.NUM_ITE_TO_VIS
+    save_name = cfg.SAVE_NAME
+    meta_name = cfg.META_NAME    
+    vis_path = cfg.VIS_PATH    
+
+    use_cuda = cfg.CUDA and torch.cuda.is_available()
+    save_path = cfg.MODEL_FOLDER
+    dataset_path = cfg.DATASET_PATH + 'CROHME2013_data/TrainINKML/expressmatch/'
+    scale_factor = cfg.TEST_SCALE_FACTOR
+    
+    # Load the vocab dictionary for display purpose
+    _, id_to_word = get_gt.build_vocab('mathsymbolclass.txt')
+
+    # Initialize the network and load its weights
+    net =  AGRU()
+    save_files = glob.glob(save_path + save_name + '*.dat')
+    if (len(save_files) > 0):
+        save_file = sorted(save_files)[-1]
+        print('Loading network weights saved at %s...' % save_file)
+        loadobj = torch.load(save_file)
+        net.load_state_dict(loadobj['state_dict']) 
+        print('Loading done.')
+
+    if (use_cuda):
+        net.cuda()
+
+    # For debugging
+    if (not is_train):
+        net.train(False)
+ 
+    # Get full paths to test inkml files, create a list of scale factors to be used for rendering test images
+    inkml_list = glob.glob(dataset_path + '*.inkml')
+    scale_list = [scale_factor] * len(inkml_list)
+    inkml_list = np.asarray(inkml_list)
+    scale_list = np.asarray(scale_list)
+    
+    #inkml_list = inkml_list[0:120]
+    #scale_list = scale_list[0:120]
+    num_test = len(inkml_list)
+    num_ite = int(np.ceil(1.0*num_test/batch_size))
+
+    # Exact match and word error rate
+    em = []
+    wer = []
+    all_pred = []
+    all_gt = []
+    # Main test loop
+    for i in range(num_ite):
+        batch_idx = range(i*batch_size, (i+1)*batch_size)
+        if (batch_idx[-1] >= num_test):
+            batch_idx = range(i*batch_size, num_test)
+        batch_size = len(batch_idx) 
+        batch_x = util.batch_data(inkml_list[batch_idx], scale_list[batch_idx], is_train)
+        batch_x = util.np_to_var(batch_x, use_cuda)
+        batch_y_np = util.batch_target(inkml_list[batch_idx])
+        batch_y = util.np_to_var(batch_y_np, use_cuda)
+        
+        #pred_y, attention = net(batch_x, batch_y)
+        pred_y, attention = net.beam_search(batch_x)
+        pred_y = util.var_to_np(pred_y, use_cuda)
+        pred_y = np.argmax(pred_y, 2)
+        batch_y = np.reshape(batch_y_np, (batch_size, max_len))
+        
+        print('Finished ite %d/%d.' % (i, num_ite)) 
+        for j in range(pred_y.shape[0]):
+            pred_string = pred_y[j,:]
+            pred_string = [id_to_word[idx] for idx in list(pred_string)]
+            gt_string = batch_y[j,:]
+            gt_string = [id_to_word[idx] for idx in list(gt_string)]
+            all_pred.append(pred_string)
+            all_gt.append(gt_string)
+            em.append(util.exact_match(pred_string, gt_string))
+            if ('</s>' in pred_string):
+                pred_string = pred_string[0:pred_string.index('</s>')+1]
+            gt_string = gt_string[0:gt_string.index('</s>')+1]
+            wer.append(util.levenshtein_distance(pred_string, gt_string))
+        
+            # Only print and visualize a fourth of them
+            if (j % 4 != 0):
+                continue
+
+            # Printing stuffs to console      
+            print('Prediction: %s' % ' '.join(pred_string))
+            print('Target: %s\n' % ' '.join(gt_string))
+                              
+            # Save attention to files for visualization
+            file_name = ntpath.basename(inkml_list[batch_idx[j]])[:-6]
+            vis_path_j = vis_path + file_name + '/'
+            if (not os.path.exists(vis_path_j)):
+                os.makedirs(vis_path_j)
+            
+            tmp_x = np.sum(batch_x.data.cpu().numpy()[j,:,:,:], axis=0)
+            attention_np = attention.data.cpu().numpy()[j,1:,:,:]
+
+            for k, word in enumerate(pred_string):
+                word = word.replace('/', 'slash_')
+                attention_k = attention_np[k,:,:]/np.max(attention_np[k,:,:])*0.8
+                attention_k = (scipy.misc.imresize(attention_k, 16.0))/255.0
+                tmp_x = scipy.misc.imresize(tmp_x, attention_k.shape)
+                attention_k += tmp_x
+                attention_k[attention_k>1] = 1
+                try:
+                    scipy.misc.imsave(vis_path_j + ('%02d_%s.jpg' % (k, word)), attention_k)
+                except FileNotFoundError:
+                    pdb.set_trace()
+                if (word == '<slash_s>'):
+                    break
+
+        #pdb.set_trace()
+
+    print("Exact match count: %d/%d" % (sum(em), len(em)))
+    print("Word error rate: %.5f" % (np.mean(wer)))
+    pdb.set_trace()
+    util.save_list([em, wer, all_pred, all_gt], save_path + test_name + '.dat')
+        
      
     pdb.set_trace() 
+
 
 
 if __name__ == '__main__':
@@ -218,4 +351,5 @@ if __name__ == '__main__':
     np.random.seed(1311)
     torch.manual_seed(1311)
 
-    train()
+    #train()
+    test()
