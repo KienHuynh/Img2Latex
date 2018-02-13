914014ad1bc46b9e0f45bf3a840f44c7a9aee45a
KienHuynh <hckien@hcmut.edu.vn>
2018-02-13 23:10:55 +0700

    Add more optionts

--- config.py
+++ config.py
@@ -11,13 +11,17 @@ LR_DECAY = 1/1.5
 NUM_EPOCH_TO_DECAY = 10
 MOMENTUM = 0.9
 MAX_GRAD_CLIP = 0.1
-BATCH_SIZE = 12
+BATCH_SIZE = 1
 RAND_TRANSFORM = True
+
+# Test settings
+BEAM_SIZE = 10
 
 # Saving
 NUM_EPOCH_TO_SAVE = 5 # Save every SAVE_STEP epoch
 SAVE_NAME = 'attend_gru_021018_weight' # MMDDYY
 META_NAME = 'attend_gru_021018_meta'
+TEST_NAME = 'attend_gru_021018_test'
 
 # For traking/displaying
 NUM_ITE_TO_LOG = 10 # Display information (loss, current epoch, etc.) every PRINT_STEP iteration
@@ -34,3 +38,4 @@ MODEL_FOLDER = './../data/trained/CROHME2013/'
 SUBSET_LIST = ['expressmatch/', 'HAMEX/', 'KAIST/', 'MathBrush/', 'MfrDB/']
 # These are the approprirate scale factors of equations came from the corresponding sub-datasets (computed using CROHME_parser.size_statistics), they are used to render numpy images from inkml data
 SCALE_FACTORS = [1.0, 100, 0.065, 0.04, 0.8] 
+TEST_SCALE_FACTOR = 0.5
