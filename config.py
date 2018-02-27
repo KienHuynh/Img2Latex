3fda9bb4fff9cc31c1723b87b6e19c4308e5f686
KienHuynh <hckien@hcmut.edu.vn>
2018-02-27 07:35:35 +0700

    Small changes

--- config.py
+++ config.py
@@ -7,25 +7,25 @@ IMW = 512
 # Training settings
 NUM_EPOCH = 500
 LR = 0.0001
-LR_DECAY = 1/1.5
-NUM_EPOCH_TO_DECAY = 30
+LR_DECAY = 1/1.3
+NUM_EPOCH_TO_DECAY = 60
 MOMENTUM = 0.9
 MAX_GRAD_CLIP = 0.1
-BATCH_SIZE = 12
+BATCH_SIZE = 1
 RAND_TRANSFORM = True
 
 # Test settings
-BEAM_SIZE = 10
+BEAM_SIZE = 12
 
 # Saving
 NUM_EPOCH_TO_SAVE = 5 # Save every SAVE_STEP epoch
-SAVE_NAME = 'attend_gru_021318_weight' # MMDDYY
-META_NAME = 'attend_gru_021318_meta'
-TEST_NAME = 'attend_gru_021318_test'
+SAVE_NAME = 'attend_gru_022018_weight' # MMDDYY
+META_NAME = 'attend_gru_022018_meta'
+TEST_NAME = 'attend_gru_022018_test'
 
 # For traking/displaying
 NUM_ITE_TO_LOG = 10 # Display information (loss, current epoch, etc.) every PRINT_STEP iteration
-NUM_ITE_TO_VIS = 250
+NUM_ITE_TO_VIS = 100
 VIS_PATH = './../vis/Image2Latex/'
 
 CUDA = True
