cd8cac10995e0a27d736b5d29eae6fe28c4a98e3
KienHuynh <hckien@hcmut.edu.vn>
2018-05-07 11:13:32 +0700

    Add resnet-like connections

--- attend_GRUv1.py
+++ attend_GRUv1.py
@@ -32,7 +32,8 @@ class AGRU(nn.Module):
         self.conv1_3_bn = nn.BatchNorm2d(32)
         self.conv1_4 = nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False)
         self.conv1_4_bn = nn.BatchNorm2d(32)
-        
+        self.projection1 = nn.Conv2d(5, 32, 1, stride=1, padding=0, bias=False)        
+
         self.pool_1 = nn.MaxPool2d(2, stride=2)
         
         self.conv2_1 = nn.Conv2d(32, 64, 3, stride=1, padding=1, bias=False)
@@ -43,7 +44,8 @@ class AGRU(nn.Module):
         self.conv2_3_bn = nn.BatchNorm2d(64)
         self.conv2_4 = nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False)
         self.conv2_4_bn = nn.BatchNorm2d(64)
-        
+        self.projection2 = nn.Conv2d(32, 64, 1, stride=1, padding=0, bias=False)  
+
         self.pool_2 = nn.MaxPool2d(2, stride=2)
         self.conv3_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1, bias=False)
         self.conv3_1_bn = nn.BatchNorm2d(128)
@@ -53,7 +55,8 @@ class AGRU(nn.Module):
         self.conv3_3_bn = nn.BatchNorm2d(128)
         self.conv3_4 = nn.Conv2d(128, 128, 3, stride=1, padding=1, bias=False)
         self.conv3_4_bn = nn.BatchNorm2d(128)
-        
+        self.projection3 = nn.Conv2d(64, 128, 1, stride=1, padding=0, bias=False)  
+
         self.pool_3 = nn.MaxPool2d(2, stride=2)
         
         self.conv4_1 = nn.Conv2d(128, 256, 3, stride=1, padding=1, bias=False)
@@ -67,9 +70,11 @@ class AGRU(nn.Module):
         self.conv4_3_drop = nn.Dropout2d(p = 0.025)
         self.conv4_4 = nn.Conv2d(256, 256, 3, stride=1, padding=1, bias=False)
         self.conv4_4_bn = nn.BatchNorm2d(256)
+        self.projection4 = nn.Conv2d(128, 256, 1, stride=1, padding=0, bias=False)  
+
         self.conv4_4_drop = nn.Dropout2d(p = 0.025)
-        self.last_conv_nc = list(self.conv4_4.parameters())[0].shape[0]
-                 
+        self.last_conv_nc = list(self.conv4_4.parameters())[0].shape[0]        
+         
         self.pool_4 = nn.MaxPool2d(2, stride=2)
 
         self.leaky_relu = nn.LeakyReLU(0.01)
@@ -113,27 +118,34 @@ class AGRU(nn.Module):
 
     def cnn_forward(self, x):
         # Compute a forward pass on convolutional layers        
+        x0 = x
         x = self.leaky_relu(self.conv1_1(x))
         x = self.leaky_relu(self.conv1_2(x))
         x = self.leaky_relu(self.conv1_3(x))
         x = self.leaky_relu(self.conv1_4(x))
-        
+        x = x + self.projection1(x0)        
+
         x = self.pool_1(x)
         
+        x0 = x
         x = self.leaky_relu(self.conv2_1(x))
         x = self.leaky_relu(self.conv2_2(x))
         x = self.leaky_relu(self.conv2_3(x))
         x = self.leaky_relu(self.conv2_4(x))
-        
+        x = x + self.projection2(x0)               
+
         x = self.pool_2(x)
          
+        x0 = x
         x = self.leaky_relu(self.conv3_1(x))
         x = self.leaky_relu(self.conv3_2(x))
         x = self.leaky_relu(self.conv3_3(x))
         x = self.leaky_relu(self.conv3_4(x))
-        
+        x = x + self.projection3(x0)               
+ 
         x = self.pool_3(x)
           
+        x0 = x
         x = self.leaky_relu(self.conv4_1(x))
         #x = self.conv4_1_drop(x)
         x = self.leaky_relu(self.conv4_2(x))
@@ -142,10 +154,12 @@ class AGRU(nn.Module):
         #x = self.conv4_3_drop(x)
         x = self.leaky_relu(self.conv4_4(x))
         #x = self.conv4_4_drop(x)
-        
+        x = x + self.projection4(x0) 
+
         fcn_result = self.pool_4(x)
 
         return fcn_result
+
 
     def forward(self, x, target):         
         x_np = np.transpose(x.data.cpu().numpy()[0,:,:,:], (1,2,0))
